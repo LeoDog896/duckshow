feea1b4afaaa2ebf5974b5774999fc36d0c5b40b
LeoDog896 <LeoDog896@hotmail.com>
2021-11-30 15:18:05 +0000

    Reordering

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script lang="ts">
-	export let name: string;
+
+	let url = "https://random-d.uk/api/1.jpg"
+
+	async function fetchAndSet(): Promise<string> {
+		let response = await fetch("https://random-d.uk/api/v2/list?format=json")
+		let json = await response.json()
+
+		return json.url
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<img src={url} alt="Duck" width="300"/>
+	<button on:click={() => fetchAndSet().then(newUrl => url = newUrl)}>New Duck</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
