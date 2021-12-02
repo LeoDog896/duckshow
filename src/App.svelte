98c56998c036f86f1ca1ea69b405d2676936e890
LeoDog896 <LeoDog896@hotmail.com>
2021-12-02 18:16:15 +0000

    Fix up fetching

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 	let url = "https://picsum.photos/500/600.webp?blur=1"
 
 	/** Utility function to fetch JSON from a url. */
-	const randomJSONAPI = (url: string): Promise<any> => fetch(url).then(data => data.json())
+	const fetchURL = (url: string): Promise<any> => fetch(url).then(data => data.json())
 	
 	/** Represents a named factory that can generate URLs of animals. */
 	interface AnimalFactory {
@@ -17,18 +17,18 @@
 	const randomAnimals: AnimalFactory[] = [
 		{
 			name: "fox",
-			url: () => randomJSONAPI("https://randomfox.ca/floof/").then(data => data.image),
+			url: () => fetchURL("https://randomfox.ca/floof/").then(data => data.image),
 			enabled: true
 		},
 		{
 			name: "bunny",
-			url: () => randomJSONAPI("https://api.bunnies.io/v2/loop/random/?media=gif")
+			url: () => fetchURL("https://api.bunnies.io/v2/loop/random/?media=gif")
 				.then(data => data.media.gif),
 			enabled: true
 		},
 		{
 			name: "dog",
-			url: () => randomJSONAPI("https://dog.ceo/api/breeds/image/random")
+			url: () => fetchURL("https://dog.ceo/api/breeds/image/random")
 				.then(data => data.message),
 			enabled: true
 		},
@@ -41,46 +41,50 @@
 			name: "cat",
 			url: async () => "https://cataas.com/cat?" + new Date(),
 			enabled: true
+		},
+		{
+			name: "raccoon",
+			url: () => fetchURL("https://some-random-api.ml/animal/raccoon")
+				.then(data => data.image),
+			enabled: true
 		}
 	]
 
-	const randomAnimal = async (): Promise<string> => {
+	const randomAnimal = (): Promise<string> | string => {
 		
 		const selectedAnimals = randomAnimals.filter(it => it.enabled)
 
 		if (selectedAnimals.length == 0) return "https://picsum.photos/500/600.webp?blur=1"
 
-		return await selectedAnimals[~~(Math.random() * selectedAnimals.length)].url()
+		return selectedAnimals[~~(Math.random() * selectedAnimals.length)].url()
 	}
 
 	/** How much time to take before grabbing another image from the factory. */
-	let timeInterval = 3000
+	let timeInterval = 3
 
 	async function showAnimal() {
 		url = await randomAnimal()
-		setTimeout(showAnimal, timeInterval)
+		setTimeout(showAnimal, Math.max(Math.min(timeInterval * 1000, 60000), 500))
 	}
 
 	showAnimal()
 
 </script>
 
-<main>
-	<div id="bigParent" style="background-image: url('{url}')"></div>
-	<div id="settings">
-		{#each randomAnimals as animal, i}
-			<div class="animal">
-				<span
-					tabindex={i + 1}
-					on:click={() => animal.enabled = !animal.enabled}
-					on:keyup={event => { if (event.key == "Enter") animal.enabled = !animal.enabled }}
-					class={animal.enabled ? "enabled" : "disabled"}
-				>{animal.name}</span>
-			</div>
-		{/each}
-		<label><span>timing</span> <input tabindex={randomAnimals.length + 1} type="number" min="500" max="50000" bind:value={timeInterval}></label>
-	</div>
-</main>
+<div id="bigParent" style="background-image: url('{url}')"></div>
+<div id="settings">
+	{#each randomAnimals as animal, i}
+		<div class="animal">
+			<span
+				tabindex={i + 1}
+				on:click={() => animal.enabled = !animal.enabled}
+				on:keydown={event => { if (event.key == "Enter") animal.enabled = !animal.enabled }}
+				class={animal.enabled ? "enabled" : "disabled"}
+			>{animal.name}</span>
+		</div>
+	{/each}
+	<label><input tabindex={randomAnimals.length + 1} bind:value={timeInterval}><span>s</span></label>
+</div>
 
 <style lang="scss">
 
@@ -101,7 +105,6 @@
 			font-family: inherit;
 			font-size: inherit;
 			padding: 3px;
-			box-sizing: border-box;
 			border: 0px solid;
 			border-bottom: 1px solid white;
 			color: white;
@@ -109,12 +112,7 @@
 			border-radius: 2px;
 			margin-left: 3px;
 			vertical-align: middle;
-		}
-
-		input::-webkit-outer-spin-button,
-		input::-webkit-inner-spin-button {
-			-webkit-appearance: none;
-			margin: 0;
+			width: 2ch;
 		}
 
 		.animal {
